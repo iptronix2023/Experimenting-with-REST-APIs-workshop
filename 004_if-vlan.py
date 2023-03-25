c04e75a61a4421b70faa0566bc1797a89db42483
iptronix2023 <126583134+iptronix2023@users.noreply.github.com>
2023-03-25 18:21:43 +0100

    Create 004_if-vlan.py

---
+++ 004_if-vlan.py
@@ -0,0 +1,7 @@
+nativeVLAN = 1
+dataVLAN = 100
+if nativeVLAN==dataVLAN:
+  print("the native VLAN and the data VLAN are the same")
+  else:
+    print("this native VLAN and the data VLAN are different")
+    
